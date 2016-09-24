e31456b89edbc8ac4ea052b56e861dbc5739dd51
andela-anwocha <adimoranma.felix@andela.com>
2016-09-24 15:37:57 +0100

    Update app.jsx

--- app/js/components/app.jsx
+++ app/js/components/app.jsx
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 class App extends React.Component {
   render(){
     return <div>
-                HelloWorld to the People of Africa and Nigeria.
+                Hello React!
            </div>
   }
 }
